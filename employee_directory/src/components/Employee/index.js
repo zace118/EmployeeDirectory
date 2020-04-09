12434c8964a5442a10629c5719cb4c02d82ce5b9
Zac Eskridge <zace118@gmail.com>
2020-04-08 19:49:27 -0700

    Added employees.json and am getting things started.

---
+++ employee_directory/src/components/Employee/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Employee extends Component {
+    render() {
+        return (
+            <div className="employee">
+                <Card />
+            </div>
+        )
+    }
+}
+
